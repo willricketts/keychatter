95c88c9f98ef426d1dd7042b284914b9ec8461a7
Will Ricketts <will@willricketts.com>
2016-01-26 12:10:02 -0500

    replacing ember with angular. adding display logic and pgp key generation

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,51 @@
-App = Ember.Application.create();
+(function() {
+  var app = angular.module('keychatter', []);
+  
+  app.controller('ConnectionController', ['$scope', '$q', function($scope, $q) {
 
-App.Router.map(function() {
-  this.route('setSelfBtcAddress', { path: '/' });
-  this.route('setTargetBtcAddress');
-  this.route('chat');
-});
+    this.user = user;
+    $scope.setSelfBtcAddress = function() {
+      user.name = $scope.name;
+      user.email = $scope.email;
+      user.pass = $scope.pass;
+      user.selfBtcAddress = $scope.selfBtcAddress;
+    }
+    
+    $scope.setTargetBtcAddress = function() {
+      user.targetBtcAddress = $scope.targetBtcAddress;
+      generateKeys(user).then(function(keys) {
+        user.pgpPubkey = keys.publicKey;
+        user.pgpPrivkey = keys.privateKey;
+      });
+    }
+    
+    function generateKeys(user) {
+      return $q(function(fulfill, reject) {
+        var opts = {
+          numBits: 2048,
+          userId: user.name + ' <' + user.email + '>',
+          passphrase: user.pass
+        };
+        
+        openpgp.generateKeyPair(opts).then(function(keypair) {
+          fulfill({
+            privateKey: keypair.privateKeyArmored,
+            publicKey: keypair.privateKeyArmored
+          });
+        }).catch(function(err) {
+          reject(err); 
+        });
+      });
+    }
+  }]);
+  
+  var user = {
+    name: '',
+    email: '',
+    pass: '',
+    selfBtcAddress: '',
+    targetBtcAddress: '',
+    pgpPubkey: '',
+    pgpPrivkey: ''
+  };
+})();
