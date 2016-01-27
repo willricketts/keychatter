114970c899497956f2ed87b3d06b713bdb4315c6
Will Ricketts <will@willricketts.com>
2016-01-27 12:04:08 -0500

    prototyping view flow and keygen

--- web/js/app.js
+++ web/js/app.js
@@ -4,11 +4,17 @@
 
     this.user = user;
     
+    $scope.loading = false;
+    $scope.keypairGenerated = false;
+    $scope.peerIdSet = false;
+    $scope.connectingToTarget = false;
+    
     $scope.setSelfBtcAddress = function() {
       user.name = $scope.name;
       user.email = $scope.email;
       user.pass = $scope.pass;
       user.selfBtcAddress = $scope.selfBtcAddress;
+      user.peerObj = setPeerId();
     }
     
     $scope.setTargetBtcAddress = function() {
@@ -17,9 +23,16 @@
       generateKeys(user).then(function(keys) {
         user.pgpPubkey = keys.publicKey;
         user.pgpPrivkey = keys.privateKey;
-        $scope.loading = false;
-        
+        $scope.keypairGenerated = true;
+        establishConnection(user);
       });
+        //$scope.loading = false;
+    }
+    
+    function establishConnection(user) {
+      console.log(user.peerObj);
+      $scope.connectingToTarget = true;
+      $scope.connectedToTarget = true;
     }
     
     function generateKeys(user) {
@@ -39,6 +52,12 @@
           reject(err); 
         });
       });
+    }
+    
+    function setPeerId() {
+      var x = new Peer(user.selfBtcAddress, {host: 'evening-waters-43284.herokuapp.com', port: 80, path: '/'});
+      $scope.peerIdSet = true;
+      return x;
     }
     
   }]);
